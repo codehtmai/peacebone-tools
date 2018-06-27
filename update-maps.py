ff6156e17fd19efeb43bbc1da6f3bc566dfd0168
cdougherty <codehtmai@gmail.com>
2018-06-26 20:50:40 -0400

    Update to allow prices to fluctuate based on wholesale delta

--- update-maps.py
+++ update-maps.py
@@ -9,6 +9,7 @@ input_product_filename = 'intermediate2.csv'
 initial_maps_filename = 'prices.csv'
 renamed_maps_filename = 'prices_{}.csv'.format(today)
 output_filename = 'products_final_{}.csv'.format(today)
+old_maps_filename = 'prices_06-11-18.csv'
 
 # Rename Prices to today
 if os.path.exists(initial_maps_filename):
@@ -37,8 +38,27 @@ for idx, line in enumerate(maps_lines):
 
     retail_val = Decimal(line[7])
     map_val = Decimal(line[8])
+    wholesale_val = Decimal(line[9])
 
-    maps[sku] = {"retail": retail_val, "map": map_val}
+    maps[sku] = {"retail": retail_val, "map": map_val, "wholesale": wholesale_val}
+
+# Old MAPs
+old_maps_reader = csv.reader(open(old_maps_filename, 'r', encoding='utf-8'))
+old_maps_lines = list(old_maps_reader)
+
+old_maps = dict()
+for idx, line in enumerate(old_maps_lines):
+    if idx == 0:
+        continue
+    sku_val = line[0]
+    sku = sku_val[1:]
+
+    retail_val = Decimal(line[7])
+    old_map_val = Decimal(line[8])
+    old_wholesale_val = Decimal(line[9])
+    old_maps[sku] = {"retail": retail_val, "map": old_map_val, "wholesale": old_wholesale_val}
+
+# End Old Maps
 
 prices_lowered = 0
 prices_raised = 0
@@ -55,9 +75,21 @@ for idx, line in enumerate(product_lines):
     price = Decimal(line[19])  # Variant Price column
     retail = maps[sku]['retail']
     map_val = Decimal(maps[sku]['map'])
+    wholesale = Decimal(maps[sku]['wholesale'])
 
-    if price < map_val:
-        prices_raised += 1
+    diff = 0
+    if sku in old_maps: # No comparison to make if it's a new SKU
+        old_wholesale = Decimal(old_maps[sku]['wholesale'])
+        diff = wholesale - old_wholesale
+        line[19] = price + diff
+
+        if diff > 0:
+            prices_raised += 1
+        elif diff < 0:
+            prices_lowered += 1
+
+    if (price + diff) < map_val:
+        print("SKU {} fell below MAP and was corrected".format(sku))
         line[19] = map_val
 
     # assert not retail < Decimal(maps[sku]['map'])
