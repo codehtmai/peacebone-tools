8771d6d29fbfec8dca099bcdccd7128a06cd6761
cdougherty <codehtmai@gmail.com>
2018-07-30 21:25:56 -0400

    Find the previous latest output by date

--- update-maps.py
+++ update-maps.py
@@ -2,6 +2,36 @@ import csv
 from decimal import *
 import datetime
 import os
+from os import listdir
+from os.path import isfile, join
+import re
+
+
+def get_date(filename, date_pattern):
+    matched = date_pattern.search(filename)
+    if not matched:
+        return None
+    m, d, y = map(int, matched.groups())
+    return datetime.date(y + 2000, m, d)
+
+
+def get_latest_filename():
+    dir_path = r"C:\Users\codeh\Documents\peacebone-tags"
+    filenames = [f for f in listdir(dir_path) if isfile(join(dir_path, f))]
+
+    date_pattern = re.compile(r'prices_(\d{2})-(\d{2})-(\d{2})\b')
+
+    dates = (get_date(fn, date_pattern) for fn in filenames)
+    dates = (d for d in dates if d is not None)
+    last_date = max(dates)
+    last_date = last_date.strftime('%m-%d-%y')
+    print(last_date)
+
+    date_filenames = [fn for fn in filenames if last_date in fn]
+    latest_prices = date_filenames[0]
+
+    return latest_prices
+
 
 today = datetime.datetime.now().strftime("%m-%d-%y")
 
@@ -9,7 +39,7 @@ input_product_filename = 'intermediate2.csv'
 initial_maps_filename = 'prices.csv'
 renamed_maps_filename = 'prices_{}.csv'.format(today)
 output_filename = 'products_final_{}.csv'.format(today)
-old_maps_filename = 'prices_06-11-18.csv'
+old_maps_filename = get_latest_filename()
 
 # Rename Prices to today
 if os.path.exists(initial_maps_filename):
